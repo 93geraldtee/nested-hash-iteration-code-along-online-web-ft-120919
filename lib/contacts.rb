2a559b7eb02832f119fea3de84bb656690211ac7
github username <github email address>
2019-12-15 18:15:07 -0500

    Done.

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -14,6 +14,61 @@ require 'pry'
   #   }
   # }
   
+
+  #You can directly iterate over the hash that is the value of the "Freddy Mercury" key by 
+  #calling an enumerator method in contacts["Freddy Mercury"].
+
+#You can set a conditional to iterate through the hash for Freddy Mercury only and when you reach 
+#the appropriate level, check to see if the key == ("is equal to") :favorite_ice_cream_flavors. 
+#If it does, check to see if that array contains "strawberry". If it does, then delete it from the Array.
+
+
 def remove_strawberry(contacts)
+  contacts.each do |name, contact_info_hash|
+    if name == "Freddy Mercury"
+      contact_info_hash.each do |attribute, data|
+          if attribute == :favorite_ice_cream_flavors
+            data.delete_if do |ice_cream|
+              ice_cream == "strawberry"
+              # binding.pry
+            end
+          end
+      end
+    end 
+  end
 
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def remove_strawberry(contacts)
+#   contacts.each do |k, v|
+#     contacts["Freddy Mercury"][:favorite_ice_cream_flavors].delete("strawberry")
+#     contacts
+#   end
+# end
